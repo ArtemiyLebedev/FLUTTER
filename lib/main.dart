923223b41952bfe55ddd73bff1d16932a8d74607
ArtemiyLebedev <kosta-kosta74@mail.ru>
2022-01-12 21:18:36 +0300

    Добавлены два файла с фоновым изображением и логотипом Durt

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,64 +20,71 @@ class MyApp extends StatelessWidget {
         color: Color(0xFF0079D0));
 
     return MaterialApp(
-      home:Scaffold(body: Container (
-        width: double.infinity,
-        child: Column(children: [
-        SizedBox(height: 70,),
-        SizedBox(width: 110, height: 84, child: Placeholder(),),
-          SizedBox(height: 23,),
-          Text('Введите логин в ввиде 10 цифр вашего телефона',
-            style:TextStyle(fontSize: 16, color: Color.fromRGBO(
-                66, 65, 65, 0.6)),),
-          SizedBox(height: 20,),
-          SizedBox( width: 224,
-            child: TextField(
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Color(0xffeceff1),
-                enabledBorder: bordereStyle,
-                focusedBorder: bordereStyle,
-                labelText: 'Телефон',
+      home:Scaffold(
+          body: Container (
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/SkyFON1.jpg'),
+                fit: BoxFit.cover,
               ),
             ),
-          ),
-      SizedBox(height: 20,),
-      SizedBox( width: 224,
-          child: TextField(
-            obscureText: true,
-            decoration: InputDecoration(
-              filled: true,
-              fillColor: Color(0xFFeceff1),
-              enabledBorder: bordereStyle,
-              focusedBorder: bordereStyle,
-              labelText: 'Пароль',
-            ),
-          ),
-      ),
-          SizedBox(height: 28,),
-          SizedBox(width: 154, height: 42, child:
-            ElevatedButton(onPressed: () {},
-              child: Text('Войти'),
-              style: ElevatedButton.styleFrom(
-                primary: Color(0xFF0079D0),
-                shape: RoundedRectangleBorder(
-                   borderRadius: BorderRadius.circular(22.0),
+            width: double.infinity,
+            height: double.infinity,
+                padding: EdgeInsets.symmetric(horizontal: 50),
+              child: SingleChildScrollView(
+                child: Column(children: [
+                  SizedBox(height: 70,),
+                  SizedBox(width: 110, height: 84, child: Image(image: AssetImage('assets/dartLOGO.png'),),),
+                  SizedBox(height: 23,),
+                  Text('Введите логин в ввиде 10 цифр вашего телефона',
+                    style:TextStyle(fontSize: 16, color:
+                    Color.fromRGBO(66, 65, 65, 0.6)),),
+                  SizedBox(height: 20,),
+                  const TextField(
+                   decoration: InputDecoration(
+                     filled: true,
+                     fillColor: Color(0xffeceff1),
+                     enabledBorder: bordereStyle,
+                     focusedBorder: bordereStyle,
+                     labelText: 'Телефон',
+                   ),
                   ),
+                  SizedBox(height: 20,),
+                  const TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xFFeceff1),
+                      enabledBorder: bordereStyle,
+                      focusedBorder: bordereStyle,
+                      labelText: 'Пароль',
+                    ),
+                  ),
+                  SizedBox(height: 28,),
+                  SizedBox(width: 254, height: 42, child:
+                  ElevatedButton(onPressed: () {},
+                    child: Text('Войти'),
+                    style: ElevatedButton.styleFrom(
+                      primary: Color(0xFF0079D0),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(22.0),
+                      ),
+                    ),
+                  ),
+                  ),
+                  SizedBox(height: 32),
+                  InkWell(
+                      child: const Text('Регистрация',
+                      style: linkTextStyle), onTap: () {}),
+                  SizedBox(height: 20,),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: InkWell(
+                        child: const Text('Забыли пароль?',style: linkTextStyle),
+                        onTap: () {}),
+                  )],
                 ),
               ),
-          ),
-          SizedBox(height: 32),
-          InkWell(child: const Text('Регистрация',
-              style: linkTextStyle), onTap: () {}),
-          SizedBox(height: 20,),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: InkWell(child: const Text('Забыли пароль?',
-              style: linkTextStyle), onTap: () {}),
-          )
-        ],),
-        )
-      ),
-    );
+          )));
   }
 }
